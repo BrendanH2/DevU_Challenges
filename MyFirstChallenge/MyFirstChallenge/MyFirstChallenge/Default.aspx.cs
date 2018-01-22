afb533938de5db542d6af875a6925d4e584156e9
Brendan Hafele <33169975+BrendanH2@users.noreply.github.com>
2018-01-22 13:17:32 -0800

    Add files via upload

---
+++ MyFirstChallenge/MyFirstChallenge/MyFirstChallenge/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            double fortune = double.Parse(fortuneTextBox.Text);
+             
+            string result = String.Format("At  {0}  years old, I would expect you to have more than  {1:C}  in your pocket.",  age, fortune);
+            //I realize I'm jumping the gun with the String.Format, but it felt it was good practice, and makes sense using a money value
+            resultLabel.Text = result;
+        }
+    }
+}
