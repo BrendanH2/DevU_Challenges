082e8ce6dfa5fef65317d5e2ac029b7ae96b6f74
Brendan Hafele <33169975+BrendanH2@users.noreply.github.com>
2018-01-23 07:47:42 -0800

    Add files via upload

---
+++ CalendarChallenge/CalendarChallenge/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CalendarChallenge.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Please Select a Date:
+            <br />
+            <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <br />
+            Please Select Another Date:<br />
+            <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Math" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CalendarChallenge/CalendarChallenge/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalendarChallenge
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            DateTime dateOne = Calendar1.SelectedDate;
+            DateTime dateTwo = Calendar2.SelectedDate;
+
+            TimeSpan result = dateTwo.Subtract(dateOne);
+
+            int numberDays = int.Parse(result.Days.ToString());
+            //converting number of days from TimeSpan into string and back to int
+
+            int absoluteDays = Math.Abs(numberDays);
+            //instead of if else, use absolute value
+
+            resultLabel.Text = absoluteDays.ToString();
+        }
+    }
+}
