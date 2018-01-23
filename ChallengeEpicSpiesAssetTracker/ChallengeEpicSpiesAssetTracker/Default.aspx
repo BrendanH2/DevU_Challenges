5ae84bd0713078381d27ed5142bae380ec033af3
Brendan Hafele <33169975+BrendanH2@users.noreply.github.com>
2018-01-23 14:36:41 -0800

    Add files via upload

---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: normal;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Image ID="logoImage" runat="server" Height="190px" />
+        <br />
+        <br />
+        <h2 class="auto-style1"><strong>Asset Performance Tracker</strong></h2>
+        <br />
+        <br />
+        Asset Name: <asp:TextBox ID="assetNameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Elections Rigged:
+        <asp:TextBox ID="electionsRiggedTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Acts of Subterfuge:
+        <asp:TextBox ID="subterfugeTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Add Asset" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            logoImage.ImageUrl = "epic-spies-logo.jpg";
+
+            if (!Page.IsPostBack)
+            {
+                double[] elections = new double[0];
+                //initialize an array, elections , with 0 elements
+                ViewState.Add("Elections", elections);
+                //Elections is a key, elections is the array
+
+                double[] subterfuge = new double[0];
+                ViewState.Add("Subterfuge", subterfuge);
+                
+            }
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            double[] elections = (double[])ViewState["Elections"];
+            //(double[]) is casting Eleections to an Array of type double
+            //Viewstate key used to access array
+            Array.Resize(ref elections, elections.Length + 1);
+            int newestItem = elections.GetUpperBound(0);
+            //in GetUpperBound, a dimesion is required. For a single array, dimension = 0
+            elections[newestItem] = double.Parse(electionsRiggedTextBox.Text);
+            ViewState["Elections"] = elections;
+
+
+            double[] subterfuge = (double[])ViewState["Subterfuge"];
+
+            Array.Resize(ref subterfuge, subterfuge.Length + 1);
+            int newestItem1 = subterfuge.GetUpperBound(0);
+            subterfuge[newestItem1] = double.Parse(subterfugeTextBox.Text);
+            ViewState["Subterfuge"] = subterfuge;
+
+            resultLabel.Text = String.Format("Total elections rigged:{0}" +
+                                             "<br />Average subterfuges completed: {1:N2} " +
+                                             "<br />(Last agent to complete a mission: {2})", 
+                                             elections.Sum(), 
+                                             subterfuge.Average(), 
+                                             assetNameTextBox.Text);
+        }
+    }
+}
