6aa5923b60333248116feef8388d6559f9222a19
Brendan Hafele <33169975+BrendanH2@users.noreply.github.com>
2018-01-23 12:05:21 -0800

    Add files via upload

---
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        &nbsp;&nbsp;&nbsp;
+            <asp:Image ID="logoImage" runat="server" Height="190px" />
+            <br />
+            <h2>Spy New Assignment Form </h2>
+            <br />
+            <br />
+            Spy Code Name:
+            <asp:TextBox ID="codeNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name:
+            <asp:TextBox ID="assignmentNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Previous Assignment:<br />
+            <asp:Calendar ID="previousAssignmentEndCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <br />
+            Start Date of New Assignment:<br />
+            <asp:Calendar ID="startNewAssignmentCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <br />
+            Projected End Date of New Assignment:
+            <br />
+            <asp:Calendar ID="projectedEndNewAssignmentCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Assign Spy" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                logoImage.ImageUrl = "epic-spies-logo.jpg";
+                previousAssignmentEndCalendar.SelectedDate = DateTime.Now.Date;
+                startNewAssignmentCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                projectedEndNewAssignmentCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
+            }
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            DateTime previousAssignmentEnd = previousAssignmentEndCalendar.SelectedDate;
+            DateTime startNewAssignment = startNewAssignmentCalendar.SelectedDate;
+            DateTime endNewAssignment = projectedEndNewAssignmentCalendar.SelectedDate;
+
+            //there must be at least two weeks of paid time between assignments
+
+            TimeSpan permissionToStart = startNewAssignment.Subtract(previousAssignmentEnd);
+            TimeSpan missionFee = endNewAssignment.Subtract(startNewAssignment);
+            String permissionToBegin = "";
+            Double fee = 0;
+            string result = "";
+
+
+            if (permissionToStart.Days >= 14)
+            {
+                
+
+                if (missionFee.Days <= 21)
+                {
+                    fee = permissionToStart.Days * 500;
+
+                    result = String.Format("{0} has permission to begin the mission: {1}. " +
+                                           "<br />{0} will depart {2: dddd, MMMM dd, yyyy } and is expected to return {3: dddd, MMMM dd, yyyy}. " +
+                                           "<br />The mission is  21 days or less and will be charged at $500 per day for an expected total of:" +
+                                           "<br />{4:C}.",
+                                           codeNameTextBox.Text,
+                                           assignmentNameTextBox.Text,
+                                           startNewAssignment,
+                                           endNewAssignment,
+                                           fee);
+
+                    resultLabel.Text = result;
+                }
+
+                else
+                {
+                    fee = permissionToStart.Days * 500 + 1000;
+
+                    result = String.Format("{0} has permission to begin the mission: {1}. " +
+                                          "<br />{0} will depart {2: dddd, MMMM dd, yyyy } and is expected to return {3: dddd, MMMM dd, yyyy}. " +
+                                          "<br />The mission is more than 21 days and will be charged at $500 per day  with an additional $1,000 " +
+                                          "<br />for an expected total of:" +
+                                          " <br />{4:C}.",
+                                          codeNameTextBox.Text,
+                                          assignmentNameTextBox.Text,
+                                          startNewAssignment,
+                                          endNewAssignment,
+                                          fee);
+
+                    resultLabel.Text = result;
+                }
+                
+            }
+            else
+            {
+                result = String.Format("{0} does not have permission to begin mission: {1}.", 
+                                                  codeNameTextBox.Text,
+                                                  assignmentNameTextBox.Text);
+
+                resultLabel.Text = result;
+            }
+
+            
+
+
+        }
+    }
+}
