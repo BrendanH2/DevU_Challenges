64d98c9e74c180dd1874e8b9073405849b35d034
Brendan Hafele <33169975+BrendanH2@users.noreply.github.com>
2018-01-25 06:50:30 -0800

    Add files via upload

---
+++ ChallengePostalCalculator/ChallengePostalCalculator/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePostalCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calcuator<br />
+            <br />
+            Width:&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            Heigth:&nbsp;
+            <asp:TextBox ID="heigthTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            Length:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingMethod" OnCheckedChanged="groundRadioButton_CheckedChanged" Text="Ground" />
+            <br />
+            <asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingMethod" OnCheckedChanged="airRadioButton_CheckedChanged" Text="Air" />
+            <br />
+            <asp:RadioButton ID="nextDayRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingMethod" OnCheckedChanged="nextDayRadioButton_CheckedChanged" Text="Next Day" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculator/ChallengePostalCalculator/Default.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            calculateRate();
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateRate();
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateRate();
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateRate();
+        }
+
+
+        private void calculateRate()
+        {
+
+
+            //   verify TextBox Inputs
+            if (!validEntry()) return;
+
+            //   find volumes
+            double volume = 0;
+            if (!validVolume(out volume)) return;
+
+            //   get shipping rate
+            double methodRate = getMethodRate();
+
+            //   calculate shipping
+            double price = volume * methodRate;
+
+            //   show rate to customer
+            resultLabel.Text = String.Format("{0:C}", price);
+        }
+ 
+        private bool validEntry()
+        {
+            if (widthTextBox.Text.Trim().Length == 0 ||
+                heigthTextBox.Text.Trim().Length == 0)
+                return false;
+            return true;
+        }
+
+        private bool validVolume(out double volume)
+        {
+            volume = 0;
+            double width = 0;
+            double heigth = 0;
+            double length = 0;
+
+            if (!double.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+            if (!double.TryParse(heigthTextBox.Text.Trim(), out heigth)) return false;
+            if (!double.TryParse(lengthTextBox.Text.Trim(), out length)) length = 1 ;
+
+            volume = width * heigth * length;
+            return true;
+        }
+
+        private double getMethodRate()
+        {
+            if (airRadioButton.Checked) return 0.25;
+            else if (nextDayRadioButton.Checked) return 0.45;
+            else return 0.15;
+        }
+
+      
+
+       
+    }
+}
