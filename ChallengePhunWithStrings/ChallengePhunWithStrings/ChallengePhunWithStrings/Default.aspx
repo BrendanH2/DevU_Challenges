03eabaf0e110da1cfe1dbf18563fe16476d0279e
Brendan Hafele <33169975+BrendanH2@users.noreply.github.com>
2018-01-29 09:16:34 -0800

    Add files via upload

---
+++ ChallengePhunWithStrings/ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePhunWithStrings.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Challenge 1</h3>
+            <br />
+            Enter a Value below<br />
+            <asp:TextBox ID="challenge1TextBox" runat="server" AutoPostBack="True" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <br />
+            <asp:Label ID="resultLabel1" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <h3>Challenge 2</h3>
+            <p>Please enter a value in each box below</p>
+            <asp:TextBox ID="challenge2TextBox1" runat="server"></asp:TextBox>
+&nbsp;
+            <asp:TextBox ID="challenge2TextBox2" runat="server"></asp:TextBox>
+&nbsp;
+            <asp:TextBox ID="challenge2TextBox3" runat="server"></asp:TextBox>
+&nbsp;
+            <asp:TextBox ID="challenge2TextBox4" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="challenge2Button" runat="server" Text="OK! Challenge 2" OnClick="challenge2Button_Click" />
+            <br />
+            <br />
+            <asp:Label ID="challenge2Label" runat="server"></asp:Label>
+            <br />
+            <br />
+            <h3>Challenge 3</h3>
+            <asp:TextBox ID="challenge3TextBox1" runat="server"></asp:TextBox>
+&nbsp;
+            <asp:TextBox ID="challenge3TextBox2" runat="server"></asp:TextBox>
+&nbsp;
+            <asp:TextBox ID="challenge3TextBox3" runat="server"></asp:TextBox>
+&nbsp;
+            <asp:TextBox ID="challenge3TextBox4" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="challenge3Button" runat="server" OnClick="challenge3Button_Click" Text="OK! Challenge 3" />
+            <br />
+            <br />
+            <asp:Label ID="challenge3Label" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <h3>Challenge 4 </h3>
+            NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID ZHEIR COUNZRY.<br />
+            <br />
+            <asp:Button ID="challenge4Button" runat="server" Text="OK! Challenge 4" OnClick="challenge4Button_Click" />
+            <br />
+            <asp:Label ID="challenge4Label" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePhunWithStrings/ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePhunWithStrings
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+         
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //initilize a blank string
+            string challenge1Solution = "";
+
+            //data from textBox
+            string textFromTextBox1 = challenge1TextBox.Text;
+
+            //output input in reverse with a for loop
+            for (int i = 0; i < textFromTextBox1.Length ; i++)
+            {
+                challenge1Solution += textFromTextBox1[textFromTextBox1.Length - i - 1 ].ToString();
+            }
+
+            resultLabel1.Text = challenge1Solution;
+                                   
+;       }
+
+
+
+        protected void challenge2Button_Click(object sender, EventArgs e)
+        {
+
+            //for challenge 2 - reverse the order an array is printed 
+            string[] challenge2Array = new string[4];
+            challenge2Array[0] = challenge2TextBox1.Text;
+            challenge2Array[1] = challenge2TextBox2.Text;
+            challenge2Array[2] = challenge2TextBox3.Text;
+            challenge2Array[3] = challenge2TextBox4.Text;
+
+            string challenge2Solution = " ";
+
+            for (int i = 0; i < challenge2Array.Length; i++)
+            {
+                challenge2Solution += challenge2Array[challenge2Array.Length - i - 1] + ", ".ToString();
+            }
+                        
+            string challenge2SolutionString = challenge2Solution.ToString();
+
+            challenge2SolutionString = challenge2SolutionString.Remove(challenge2SolutionString.Length- 2, 1);
+
+            challenge2Label.Text = challenge2SolutionString;
+
+
+        }
+
+        protected void challenge3Button_Click(object sender, EventArgs e)
+        {
+            //use PadLeft and PadRight 
+
+            string[] challenge3Array = new string[4];
+
+            challenge3Array[0] = challenge3TextBox1.Text;
+            challenge3Array[1] = challenge3TextBox2.Text;
+            challenge3Array[2] = challenge3TextBox3.Text;
+            challenge3Array[3] = challenge3TextBox4.Text;
+
+            string challenge3SolutionString = " ";
+            string challenge3Solution = " ";
+
+            int paddingLeft = 0;
+            int paddingRight = 0;
+
+            for (int i = 0; i < challenge3Array.Length; i++)
+            {
+                paddingLeft = 12 - (12 - challenge3Array[i].Length)/2  ;
+                paddingRight = 12 ;
+                challenge3Solution = challenge3Array[i].PadLeft(paddingLeft , '*' ).PadRight(paddingRight, '*') + "<br/>";
+               
+                challenge3SolutionString += challenge3Solution;
+
+                challenge3Label.Text = challenge3SolutionString;
+            }
+
+            /*
+            paddingLeft = (12 - challenge3Array[i].Length) / 2;
+            paddingRight = 12 - challenge3Array[i].Length - paddingLeft;
+            */
+
+        }
+
+        protected void challenge4Button_Click(object sender, EventArgs e)
+        {
+            string stringToConvert = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID ZHEIR COUNZRY.";
+            string ammendedString = stringToConvert.ToLower();
+            ammendedString = ammendedString.Replace('z', 't').Replace("now", "Now").Remove(10, 9);
+            ammendedString = ammendedString;
+
+        
+            challenge4Label.Text = ammendedString;
+        }
+    }
+}
